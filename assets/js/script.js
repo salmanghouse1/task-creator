1a87bcbeeeb904681e05e761fce3883700f7492e
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-02 16:10:28 -0400

    added task list input functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 // variable expression as a function
 var createTaskHandler = function(event) {
     event.preventDefault();
-    var listItemEl = document.createElement('li');
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task.";
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskNameInputSelect = document.querySelector("select[name='task-type']").value;
 
+    // Create a list item
+    var listItemEl = document.createElement('li');
+    // Create a div to hold the content
+    var taskInfoEl = document.createElement('div');
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add html content to div
+    taskInfoEl.innerHTML("<h3 class='task-name'>" + taskTypeInput + "</h3><span class='task-type'>" + taskNameInput + "</span>");
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 
 }
